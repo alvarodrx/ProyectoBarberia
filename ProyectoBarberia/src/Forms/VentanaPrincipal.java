b0ea93f7f045de65b5878bea1a9c0757e05c019f
tanzanita <ttanzanita@gmail.com>
2016-04-24 15:18:27 -0600

    Ventana principal con botones

--- ProyectoBarberia/src/Forms/VentanaPrincipal.java
+++ ProyectoBarberia/src/Forms/VentanaPrincipal.java
@@ -27,21 +27,160 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Lbarberia = new javax.swing.JLabel();
+        BotonCliente = new javax.swing.JButton();
+        BotonCitas = new javax.swing.JButton();
+        BotonServicios = new javax.swing.JButton();
+        BotonHorario = new javax.swing.JButton();
+        BotonListaEspera = new javax.swing.JButton();
+        BotonAgregar = new javax.swing.JButton();
+        BotonEliminar = new javax.swing.JButton();
+        BotonEditar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Lbarberia.setText("Barberia");
+
+        BotonCliente.setText("Cliente");
+        BotonCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonClienteActionPerformed(evt);
+            }
+        });
+
+        BotonCitas.setText("Citas");
+        BotonCitas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonCitasActionPerformed(evt);
+            }
+        });
+
+        BotonServicios.setText("Servicios");
+        BotonServicios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonServiciosActionPerformed(evt);
+            }
+        });
+
+        BotonHorario.setText("Horario");
+        BotonHorario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonHorarioActionPerformed(evt);
+            }
+        });
+
+        BotonListaEspera.setText("Lista Espera");
+        BotonListaEspera.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonListaEsperaActionPerformed(evt);
+            }
+        });
+
+        BotonAgregar.setText("+");
+
+        BotonEliminar.setText("-");
+        BotonEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonEliminarActionPerformed(evt);
+            }
+        });
+
+        BotonEditar.setText("*");
+        BotonEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonEditarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(BotonEditar))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(181, 181, 181)
+                        .addComponent(Lbarberia)
+                        .addGap(0, 109, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(BotonCitas)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(BotonEliminar))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(BotonCliente)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(BotonAgregar))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(BotonServicios)
+                                    .addComponent(BotonHorario)
+                                    .addComponent(BotonListaEspera))
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addGap(70, 70, 70))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Lbarberia)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(39, 39, 39)
+                        .addComponent(BotonCliente))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(BotonAgregar)))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BotonCitas)
+                    .addComponent(BotonEliminar))
+                .addGap(18, 18, 18)
+                .addComponent(BotonServicios)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(BotonEditar)
+                .addGap(5, 5, 5)
+                .addComponent(BotonHorario)
+                .addGap(18, 18, 18)
+                .addComponent(BotonListaEspera)
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BotonClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonClienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonClienteActionPerformed
+
+    private void BotonCitasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonCitasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonCitasActionPerformed
+
+    private void BotonServiciosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonServiciosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonServiciosActionPerformed
+
+    private void BotonHorarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonHorarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonHorarioActionPerformed
+
+    private void BotonListaEsperaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonListaEsperaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonListaEsperaActionPerformed
+
+    private void BotonEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonEliminarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonEliminarActionPerformed
+
+    private void BotonEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonEditarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonEditarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +218,14 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotonAgregar;
+    private javax.swing.JButton BotonCitas;
+    private javax.swing.JButton BotonCliente;
+    private javax.swing.JButton BotonEditar;
+    private javax.swing.JButton BotonEliminar;
+    private javax.swing.JButton BotonHorario;
+    private javax.swing.JButton BotonListaEspera;
+    private javax.swing.JButton BotonServicios;
+    private javax.swing.JLabel Lbarberia;
     // End of variables declaration//GEN-END:variables
 }
